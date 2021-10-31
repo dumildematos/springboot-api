6cc095342fc8d0da006e11dbdee289c618f82b73
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-31 15:08:21 +0100

    add delete and update services

--- src/main/java/com/amigoscode/springbootapi/student/StudentService.java
+++ src/main/java/com/amigoscode/springbootapi/student/StudentService.java
@@ -3,8 +3,11 @@ package com.amigoscode.springbootapi.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,6 +24,37 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
 
+        if(studentByEmail.isPresent()){
+            throw new IllegalStateException("Email Taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id "+ studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id "+ studentId + " does not exists"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student>studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email Taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
